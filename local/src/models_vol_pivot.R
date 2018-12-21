6cc1b609986e8a2608152feaf58d36c49221c89a
Elena Grassi <grassi.e@gmail.com>
2018-12-21 15:27:21 +0100

    Models with gene desc and median of deltavol, cetuxi and other mutational data

--- local/src/models_vol_pivot.R
+++ local/src/models_vol_pivot.R
@@ -34,6 +34,7 @@ nmf <- colSums(mutdatafeas)
 wanted <- names(nmf[nmf>=n_mutated])
 mutdatafeas <- mutdatafeas[,colnames(mutdatafeas) %in% wanted]
 
+# Fix  TODO output real n. of MSI with mutations!
 msi <- read.table(msi, sep="\t", header=FALSE)
 averages[,"msi"] <- rep(0, nrow(averages))
 averages[rownames(averages) %in% msi[,1],"msi"] <- 1
@@ -60,13 +61,18 @@ onemodel <- function(mut, avg, samples) {
     df2 <- sm$fstatistic[3]
     pvF <- pf(f,df1,df2,low=F)
     coef <- sm$coefficients[,4]
-    pvT <- coef[names(coef) == "status"]
+    if (any(names(coef) == "status")) {
+        pvT <- coef[names(coef) == "status"]
+    }
   }
-  return(c(sm$r.squared, pvF, pvT))
+  medianmut <- median(m[m$status==1, "perc"])
+  medianwt <- median(m[m$status==0, "perc"])
+  
+  return(c(sm$r.squared, pvF, pvT, medianmut, medianwt))
 }
 
 res <- as.data.frame(t(as.data.frame(apply(mutdatafeas, 2, onemodel, averages, rownames(mutdatafeas)))))
-colnames(res) <- c("R2","pval_global", "pval_gene")
+colnames(res) <- c("R2","pval_global", "pval_gene", "medianmut", "medianwt")
 res$adj_pval_global <- p.adjust(res$pval_global, method="BH")
 res$adj_pval_gene <- p.adjust(res$pval_gene, method="BH")
 res <- res[order(res$pval_gene),]
@@ -74,15 +80,21 @@ write.table(res, file=linearOut, sep="\t", quote=FALSE)
 
 #mres <- merge(res, annot, by.x="row.names", by.y="gs")
 mres <- res
-library ("AnnotationDbi")
-library("org.Hs.eg.db") # very generic indeed! Bravo Elena!
-mres$desc <- mapIds(org.Hs.eg.db,keys=row.names(mres), column="GENENAME", keytype="SYMBOL", multiVals="first") # first chooses the first value, for description
-# should be good enough.
-# we need to put the last column of mres as second one, the number of columns is fixed but I'm lazy to check by hand
-last <- ncol(mres)
-others <- seq(1, last-1)
-mres <- mres[,c(last, others)]
-
+if (snakemake@wildcards[[1]] != "genepathways") {
+    library ("AnnotationDbi")
+    library("org.Hs.eg.db") # very generic indeed! Bravo Elena!
+    if (grepl("cnv",snakemake@wildcards[[1]])) {
+        gs <- sapply(strsplit(rownames(mres),"_"), function(x) {x[1]})
+        mres$desc <- mapIds(org.Hs.eg.db,keys=gs, column="GENENAME", keytype="SYMBOL", multiVals="first") # first chooses the first value, for description 
+    } else {
+        mres$desc <- mapIds(org.Hs.eg.db,keys=row.names(mres), column="GENENAME", keytype="SYMBOL", multiVals="first") # first chooses the first value, for description 
+    }
+    # should be good enough.
+    # we need to put the last column of mres as second one, the number of columns is fixed but I'm lazy to check by hand
+    last <- ncol(mres)
+    others <- seq(1, last-1)
+    mres <- mres[,c(last, others)]
+}
 WriteXLS(mres, ExcelFileName = linearOutXls, row.names = TRUE, col.names = TRUE) 
 
 plots <- function(wanted, mut, avg) {
@@ -109,7 +121,10 @@ classmodel <- function(mut, avg, samples) {
   sm <- summary(model)
   pv <- 1 - pchisq(sm$null.deviance-sm$deviance, df=sm$df.null-sm$df.residual)
   coef <- sm$coefficients[,4]
-  pvT <- coef[names(coef) == "status"]
+  pvT <- NA
+  if (any(names(coef) == "status")) {
+    pvT <- coef[names(coef) == "status"]
+  }
   return(c(sm$aic, pv, pvT))
 }
 res2 <- as.data.frame(t(as.data.frame(apply(mutdatafeas, 2, classmodel, averages, rownames(mutdatafeas)))))
