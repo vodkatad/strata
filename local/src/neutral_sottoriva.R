207cb0ad42095b799f7b2e8ac28bbb2168c73925
Elena Grassi <grassi.e@gmail.com>
2019-01-15 18:01:43 +0100

    WIP on plots to detect neutrality (?!), pon and paired tries

---
+++ local/src/neutral_sottoriva.R
@@ -0,0 +1,29 @@
+loweraf <- snakemake@params[["loweraf"]]
+data <- snakemake@input[["afmatrix"]]
+afcolumn <- snakemake@params[["afcolumn"]]
+fit <- snakemake@output[["fit"]]
+histo <- snakemake@output[["hist"]]
+debug <- snakemake@params[["debug"]]
+
+if (debug == "yes") {
+  save.image(file=paste0(fit,'.debug','.RData'))
+}
+
+data <- read.table(gzfile(data), sep="\t", header=TRUE)
+af <- data[,afcolumn, drop=FALSE]
+
+exsubcl <- af[af<loweraf & af != 0]
+excum <- sapply(1:length(exsubcl),function(i)sum(exsubcl[i]<=exsubcl[1:length(exsubcl)]))
+invf <- 1/exsubcl
+
+pdf(fit)
+plot(invf, excum)
+dev.off()
+pdf(histo)
+hist(exsubcl, breaks=50)
+dev.off()
+
+if (debug == "yes") {
+  save.image(file=paste0(fit,'.debug','.RData'))
+}
+
