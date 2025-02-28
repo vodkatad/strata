1532a172dfcb1f8e402ad359eda0524e15bd4278
Elena Grassi <grassi.e@gmail.com>
2025-02-28 18:36:03 +0100

    IHC corrplots with list for xy-lim and axis lettering

--- local/src/ihc_corrplot.R
+++ local/src/ihc_corrplot.R
@@ -8,6 +8,20 @@ log_f <- snakemake@log[['log']]
 size <- 8
 
 
+axis_names <- list(
+  H2AX_induction= 'γ-2AX induction post-treatment',
+  dvw3= 'Relative tumor growth (%t baseline)'
+)
+
+x_lim <- list(
+  H2AX_induction= c(0, 60)
+)
+
+y_lim <- list(
+  dvw3 = c(-100, 300)
+)
+
+
 m <- read.table(m_f, sep="\t", stringsAsFactors = F)
 
 #font_add(family = "myriad", regular = snakemake@input[['myriad']])
@@ -26,8 +40,13 @@ death_conversion_dpi96 = 96/72
 textSize <- size * death_conversion_dpi96
 largerSize <- (size) * death_conversion_dpi96
 
+# arial family gave:
+#Error in grid.Call.graphics(C_text, as.graphicsAnnot(x$label), x$x, x$y,  : 
+#                              invalid font type
+                            
+
 unmute_theme <- theme(
-  text = element_text(size = textSize, family='Arial'),
+  text = element_text(size = textSize), #, family='Arial'),
   axis.title = element_text(size = largerSize),
   axis.text.x = element_text(size = textSize, color="black"),#, angle = 90, vjust = 0.5, hjust=1)
   axis.text.y = element_text(size = textSize, color="black"),
@@ -56,12 +75,16 @@ guess_ticks <- function(values, nticks=5, fixed_max=NULL, fixed_min=0) {
 plot_lmsmooth_info <- function(xvar, yvar, md) {
   pear <- cor.test(md[,xvar], md[,yvar])
   title <- paste0("pearson=", signif(pear$estimate, 3), " P=", signif(pear$p.value, 3))
-  y_breaks <- guess_ticks(md[,yvar])
-  x_breaks <- guess_ticks(md[,xvar])
+  xl <- x_lim[[xvar]]
+  yl <- y_lim[[yvar]]
+  y_breaks <- guess_ticks(md[,yvar], fixed_min=yl[1], fixed_max=yl[2])
+  x_breaks <- guess_ticks(md[,xvar], fixed_min=xl[1], fixed_max=xl[2])
   p <- ggplot(data=md, aes_string(x=xvar, y=yvar))+geom_point(size=1)+theme_bw()+geom_smooth(method="lm", se=FALSE, size=1)+
     unmute_theme+theme(legend.position="none") + xlab(xvar)+ylab(yvar)+
-    scale_y_continuous(breaks=y_breaks, limits=c(min(y_breaks),max(y_breaks)), expand=c(0,0))+
-    scale_x_continuous(breaks=x_breaks, limits=c(min(x_breaks),max(x_breaks)), expand=c(0,0))
+    scale_y_continuous(breaks=y_breaks, limits=yl, expand=c(0,0))+
+    scale_x_continuous(breaks=x_breaks, limits=xl, expand=c(0,0))+
+    xlab(axis_names[[xvar]])+
+    ylab(axis_names[[yvar]])
   return(list(p, title))
 }
 
@@ -75,3 +98,5 @@ sink()
 #ggsave(p, file=plot_cor_png, width=89*(death_conversion_dpi96), height=89*(death_conversion_dpi96), units="mm")
 
 ggsave(p[[1]], file=outplot_f, width=89*(death_conversion_dpi96), height=89*(death_conversion_dpi96), units="mm")
+
+save.image('pippo.Rdata')
